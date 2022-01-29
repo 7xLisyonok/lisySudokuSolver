1661c4a64749dbf62969bd1845b89ef146cf5f10
7xLisyonok <7x.Lisyonok@gmail.com>
2022-01-30 09:15:27 +1000

    export PuzzleView, split PuzzleViewText

--- src/PuzzleView.ts
+++ src/PuzzleView.ts
@@ -4,11 +4,14 @@ import { PuzzleCell } from "./PuzzleCell.js";
 const IS_CELL_REG = /[0-9A-Za-z\.]/;
 const IS_CELL_EMPTY = /[\.]/;
 
-type TemplatePart = {
+type TextTemplatePart = {
     type: 'raw' | 'cell';
     value: string;
     cell?: PuzzleCell;
 }
+
+export const numberToChar = (n: number) => n.toString(36).toUpperCase();
+export const charToNumber = (c: string) => Number.parseInt(c, 36);
 
 export function readArrayNumber(originalString: string | Array<number>): Array<number> {
     if (typeof originalString !== 'string') return originalString;
@@ -17,29 +20,24 @@ export function readArrayNumber(originalString: string | Array<number>): Array<n
         .split('')
         .filter(e => IS_CELL_REG.test(e))
         .map(e => IS_CELL_EMPTY.test(e) ? '0' : e)
-        .map(e => Number.parseInt(e, 36))
+        .map(charToNumber)
     ;        
 }
 
-export class PuzzleViewText {    
+export class TextTemplate {
     private _templateOriginal: string;
-    private _template: Array<TemplatePart>;
-    private _puzzle: Puzzle;
-    //private _cells
+    private _template: Array<TextTemplatePart>;
 
-    constructor(template: string, puzzle: Puzzle) {
-        this._puzzle = puzzle;
+    constructor(template: string) {
         this._templateOriginal = template;
-        const templateArr = PuzzleViewText.trimTemplate(template).split('');
+        const templateArr = TextTemplate.trimTemplate(template).split('');
 
         let cellIndex = 0;
-        this._template = templateArr.map<TemplatePart>(char => {
+        this._template = templateArr.map<TextTemplatePart>(char => {
             if (PuzzleViewText.isCellChar(char)) {
-                
                 return {
                     type: 'cell',
                     value: null,
-                    cell: puzzle.field[cellIndex++]
                 }
             }
 
@@ -50,17 +48,60 @@ export class PuzzleViewText {
         });
     }
 
+    render(values: Array<string | number>): string {
+        let templateIndex = 0;
+        const preparedArr = this._template.map((part, index) => {
+            const value =
+                (part.type === 'cell') ?
+                values[templateIndex++] :
+                part.value;
+
+            if (typeof value === 'number') return numberToChar(value);
+            return value;
+        });
+
+        return preparedArr.join('');
+    }
+
+    static trimTemplate(template: string): string {
+        const templateLines = template.split('\n').filter(line => line.trim().length > 0);
+        
+        const templateLinesInfo = templateLines.map(line => {
+            const trimLine = line.trim();
+            return {
+                spaceCount: line.indexOf(trimLine),
+                length: trimLine.length,
+            }
+        });
+    
+        const minSpaceCount = Math.min.apply(null, templateLinesInfo.map(line => line.spaceCount));
+        const maxLineLength = Math.max.apply(null, templateLinesInfo.map(line => line.length));
+        const templplateLinesClear = templateLines.map((line) => {
+            return line.substring(minSpaceCount, maxLineLength + minSpaceCount);
+        });
+    
+        return templplateLinesClear.join('\n');
+    }     
+}
+
+export class PuzzleViewText {    
+    private _puzzle: Puzzle;
+    private _textTemplate: TextTemplate;
+    //private _cells
+
+    constructor(template: string, puzzle: Puzzle) {
+        this._puzzle = puzzle;
+        const templateArr = TextTemplate.trimTemplate(template).split('');
+        this._textTemplate = new TextTemplate(template);
+    }
+
     static isCellChar(char: string) {
         return IS_CELL_REG.test(char);
     }
 
     render(): string {
-        const preparedArr = this._template.map((part, index) => {
-            if (part.type === 'cell') return part.cell.valueChar;
-            return part.value;
-        });
-
-        return preparedArr.join('');
+        const values = this._puzzle.field.map(cell => cell.valueChar);
+        return this._textTemplate.render(values);
     }
 
     renderBlocks(): string {
@@ -83,26 +124,5 @@ export class PuzzleViewText {
 
         const renderResult = renderResultArr.join('\n').replace(/1/g, '\x1b[33m#\x1b[0m');
         return renderResult;
-    }
-
-    static trimTemplate(template: string): string {
-        const templateLines = template.split('\n').filter(line => line.trim().length > 0);
-        
-        const templateLinesInfo = templateLines.map(line => {
-            const trimLine = line.trim();
-            return {
-                spaceCount: line.indexOf(trimLine),
-                length: trimLine.length,
-            }
-        });
-    
-        const minSpaceCount = Math.min.apply(null, templateLinesInfo.map(line => line.spaceCount));
-        const maxLineLength = Math.max.apply(null, templateLinesInfo.map(line => line.length));
-        const templplateLinesClear = templateLines.map((line) => {
-            return line.substring(minSpaceCount, maxLineLength + minSpaceCount);
-        });
-    
-        return templplateLinesClear.join('\n');
-    }
-    
+    }   
 }
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./Puzzle.js";
 export * from "./PuzzleSolver.js";
 export * from "./PuzzleBlock.js";
 export * from "./PuzzleCell.js";
+export * from "./PuzzleView.js";
